b9702cc76e05f47268956cf9ee24fe7e191c01e1
India Kerle <india.kerle@nesta.org.uk>
2021-12-15 09:53:25 +0000

    deleted files

--- mapping-career-causeways/mapping_career_causeways/scripts/upskilling_aws_scripts/skill_perturbation_All_to_essential_Batch_4.sh
+++
@@ -1,5 +0,0 @@
-echo "Bash version ${BASH_VERSION}..."
-for i in {60..79}
-do
-  python3 skill_perturbation_All_to_essential_C.py $i
-done
--- mapping-career-causeways/mapping_career_causeways/scripts/upskilling_aws_scripts/skill_perturbation_essential_Batch_1.sh
+++
@@ -1,5 +0,0 @@
-echo "Bash version ${BASH_VERSION}..."
-for i in {0..49}
-do
-  python3 skill_perturbation_essential_C.py $i
-done
